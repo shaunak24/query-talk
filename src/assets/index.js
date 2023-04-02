2e3a1e455287f6774617833a7641d87d8605dba2
Shaunak <shaunak241997@gmail.com>
2023-04-02 21:10:26 +0530

    Add copy and generate btn

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import close from './close.svg';
 import menu from './menu.svg';
 import logo from './logo.png';
 import switchImg from './switch.svg';
+import copy from './copy.svg';
 
-export { logo, menu, close, switchImg };
+export { logo, menu, close, switchImg, copy };
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { copy } from '../assets';
 
 const Panel = ({ label, name, placeholder, text, setText }) => {
   return (
-    <div className="w-full">
+    <div className="w-full bg-orange-100 p-4 rounded-lg">
       <label className="flex flex-col">
         <span className="font-medium mb-4 text-secondary">{label}</span>
         <textarea
@@ -14,6 +15,18 @@ const Panel = ({ label, name, placeholder, text, setText }) => {
           className="py-4 px-6 placeholder:text-secondary outline-none border-none rounded-lg text-black font-medium shadow-2xl sm:h-96"
         />
       </label>
+      {name === 'leftPanel' && (
+        <button className="bg-primary rounded-lg mt-4 p-2 text-secondary cursor-pointer">
+          Generate
+        </button>
+      )}
+      {name === 'rightPanel' && (
+        <img
+          src={copy}
+          alt="copy to clipboard"
+          className="bg-primary rounded-lg mt-4 p-2 text-secondary cursor-pointer"
+        />
+      )}
     </div>
   );
 };
